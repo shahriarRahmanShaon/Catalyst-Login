3db30d3d4ed3dfca83032d95573cef33a1bad47b
shahriarRahmanShaon <shaon463@gmail.com>
2021-05-04 15:02:47 +0600

    Initial Commit

---
+++ Shared/Catalyst_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catalyst_LoginApp.swift
+//  Shared
+//
+//  Created by sergio shaon on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Catalyst_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
